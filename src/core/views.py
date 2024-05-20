a864b6afaf96cb8c94b1f1987e791b13c89b6833
Jacobo Escorcia <jacoboe127@gmail.com>
2024-05-19 18:57:34 -0600

    Vista principal creada

--- src/core/views.py
+++ src/core/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "base.html", {})
+    context = {}
+
+    context['type_investor'] = 'Moderado'
+
+    return render(request, "home.html", context)
